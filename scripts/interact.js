8808c86d052366061092a3039bc1e5181917f335
hemantjuyal <hemantjuyal@gmail.com>
2022-06-04 22:09:53 +0530

    adding dependency

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,9 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-
+const {
+  ethers
+} = require("hardhat");
 const contract = require("../artifacts/contracts/GreetingMessages.sol/GreetingMessages.json");
 
 console.log(JSON.stringify(contract.abi));
