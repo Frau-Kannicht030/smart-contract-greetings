73aaed610949220bea963deaa35b8312fd668bd5
hemantjuyal <hemantjuyal@gmail.com>
2022-06-04 22:46:04 +0530

    smart contract etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,15 @@ require('dotenv').config({
   path: __dirname + '/.env'
 });
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const {
   API_URL,
   API_KEY,
   PRIVATE_KEY
 } = process.env;
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.7.3",
@@ -23,4 +26,7 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`]
     }
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
